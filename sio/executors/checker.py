c7c6fa2ee5ede63b63713d8a9baae168aa12b7e1
oi-admins <oi-admin@dasie.mimuw.edu.pl>
2020-03-21 11:30:58 +0000

    [tmpfix] Increase checker memory to 268 MiB

--- sio/executors/checker.py
+++ sio/executors/checker.py
@@ -11,7 +11,7 @@ from sio.workers.util import tempcwd
 logger = logging.getLogger(__name__)
 
 DEFAULT_CHECKER_TIME_LIMIT = 30000  # in ms
-DEFAULT_CHECKER_MEM_LIMIT = 256 * 2**10  # in KiB
+DEFAULT_CHECKER_MEM_LIMIT = 268 * 2**10  # in KiB
 RESULT_STRING_LENGTH_LIMIT = 1024  # in bytes
 
 class CheckerError(Exception):
