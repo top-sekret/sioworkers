e2fe79e352d0e9f3e3f4d042eba1a7230a503553
MasloMaslane <matimasiarz@gmail.com>
2024-06-30 21:17:48 +0200

    Another fix

--- sio/workers/executors.py
+++ sio/workers/executors.py
@@ -565,9 +565,10 @@ class _SIOSupervisedExecutor(SandboxExecutor):
         renv['result_code'] = result_code
         renv['return_code'] = exit_code
 
-        renv['exit_signal'] = int(
-            renv['result_string'][len('process exited due to signal '):]
-        )
+        if str(renv['result_string']).startswith('process exited due to signal'):
+            renv['exit_signal'] = int(
+                str(renv['result_string'])[len('process exited due to signal '):]
+            )
 
         if result_code != 'OK' and not ignore_errors and not \
                 (result_code == 'RE' and renv['return_code'] in \
