5fb6d188d895a489e69dbf51b2954f7c27accf52
MasloMaslane <matimasiarz@gmail.com>
2025-08-24 10:09:11 +0200

    Change command name

--- setup.py
+++ setup.py
@@ -80,7 +80,7 @@ setup(
             'default-ml = sio.compilers.ocaml:run_ocaml',
             'default-nasm = sio.compilers.nasm:run',
             'default-gas = sio.compilers.gas:run',
-            'default-speech-to-text = sio.compilers.speech_to_text:run',
+            'default-mp3 = sio.compilers.speech_to_text:run',
 
             # Sandboxed compilers:
             'c = sio.compilers.gcc:run_gcc',
